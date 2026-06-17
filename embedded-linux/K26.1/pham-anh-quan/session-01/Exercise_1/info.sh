589e2b9e7bee2be1a87df70c459a1377f37b69bd
Pham Anh Quan <anhquantph@gmail.com>
2026-06-17 12:07:14 +0700

    fix Exercise_1 format and rename answers.md for Exercise_4

--- embedded-linux/K26.1/pham-anh-quan/session-01/Exercise_1/info.sh
+++ embedded-linux/K26.1/pham-anh-quan/session-01/Exercise_1/info.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Print the current logged-in user
 echo "Hello, $USER!"
+
+# Print the current working directory path
+echo "Current directory: $(pwd)"
+
+# Print the home directory path of the current user
 echo "Your home directory is: $HOME"
+
+# Print the path of the shell session currently in use
 echo "Your shell is: $SHELL"
