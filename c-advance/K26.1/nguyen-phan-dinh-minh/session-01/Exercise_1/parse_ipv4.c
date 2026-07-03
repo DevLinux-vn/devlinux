1a31a98d72da49f4e17810f729ad452345cefb22
Minh-207 <hungxaunhucho2209@gmail.com>
2026-07-03 17:52:33 +0700

    Update parse_ipv4

--- c-advance/K26.1/nguyen-phan-dinh-minh/session-01/Exercise_1/parse_ipv4.c
+++ c-advance/K26.1/nguyen-phan-dinh-minh/session-01/Exercise_1/parse_ipv4.c
@@ -14,16 +14,16 @@ int8_t parse_ipv4(const char *ip_str, uint32_t *p_ip_out)
     for(int i = 0; ip_str[i] != '\0'; i++)
     {
         char ch = ip_str[i];
-        //(c) - '0'; Mã ASCII thì lấy mã ASCII của số cần trừ mốc '0' thì ra được số cần gọi là quy đổi từ kí tự sang số 
-        //biến từng kí tự thành số
-        if( (ch) >= '0' && (ch) <= '9') //Nếu kí tự là chữ số
+        /*(c) - '0'; Mã ASCII thì lấy mã ASCII của số cần trừ mốc '0' thì ra được số cần gọi là quy đổi từ kí tự sang số */
+        /*biến từng kí tự thành số*/
+        if( (ch) >= '0' && (ch) <= '9') /*Nếu kí tự là chữ số*/
         {
             if ( (part) == 0 ) a = a * 10 + ( (ch) - '0');
             else if ( (part) == 1) b = b * 10 + ( (ch) - '0');
             else if ( (part) == 2) c = c * 10 + ( (ch) - '0');
             else d = d * 10 + ( (ch) - '0');
         } 
-        else if ( (ch) == '.')
+        else if ( (ch) == '.') /*gặp dấu chấm là bỏ qua*/
         {
             part++;
             if (part > 3) return -1;
