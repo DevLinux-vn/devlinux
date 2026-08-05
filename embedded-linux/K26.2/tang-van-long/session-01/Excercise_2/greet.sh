87fb83b5e27e91911cab56d88a19acf5e8863236
Tang Van Long <longtang241@gmail.com>
2026-08-05 23:52:23 +0700

    embedded-linux K26.2 session-01: hoàn thành Exercise_1, Exercise_2, Exercise_3, Exercise_4

---
+++ embedded-linux/K26.2/tang-van-long/session-01/Excercise_2/greet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -ge 2 ];
+then
+	echo "Hello, my name is $1 and I am $2 years old"
+	echo "Total arguments received: $#"
+else
+	echo "Usage: Please enter 2 arguments: <name> <age>"
+	exit 1
+fi
+
+exit 0
