f1c5ba1a47cdd12fb1d7eeabd56fe364bb79897f
danh <danhgit50@gmail.com>
2026-08-09 17:23:29 +0700

    embedded-linux K26.2 session-02: hoàn thành Exercise_1, Exercise_2, Exercise_3

--- embedded-linux/K26.2/doan-cong-danh/session-02/Exercise_1/app/main.c
+++ embedded-linux/K26.2/doan-cong-danh/session-02/Exercise_1/app/main.c
@@ -7,4 +7,5 @@ int main(){
     printf("Addition of %d and %d is: %d\n", a, b, math_add(a, b));
     printf("Subtraction of %d and %d is: %d\n", a, b, math_sub(a, b));
     printf("Factorial of %d is: %d\n", n, math_factorial(n));
+    return 0;
 }
