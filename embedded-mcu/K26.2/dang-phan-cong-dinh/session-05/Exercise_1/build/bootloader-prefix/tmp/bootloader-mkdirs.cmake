a5a5bd7eac22aa988cacf9423318aad060b7ce55
dangphancongdinh-max <dang.phancongdinh@gmail.com>
2026-09-06 00:00:27 +0700

    embedded-mcu K26.2 session-05: hoàn thành Exercise_1

---
+++ embedded-mcu/K26.2/dang-phan-cong-dinh/session-05/Exercise_1/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Users/hanph/esp/esp-idf/components/bootloader/subproject"
+  "C:/Users/hanph/ESP32S3/05_UART/Exercise_1/build/bootloader"
+  "C:/Users/hanph/ESP32S3/05_UART/Exercise_1/build/bootloader-prefix"
+  "C:/Users/hanph/ESP32S3/05_UART/Exercise_1/build/bootloader-prefix/tmp"
+  "C:/Users/hanph/ESP32S3/05_UART/Exercise_1/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/Users/hanph/ESP32S3/05_UART/Exercise_1/build/bootloader-prefix/src"
+  "C:/Users/hanph/ESP32S3/05_UART/Exercise_1/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Users/hanph/ESP32S3/05_UART/Exercise_1/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Users/hanph/ESP32S3/05_UART/Exercise_1/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
