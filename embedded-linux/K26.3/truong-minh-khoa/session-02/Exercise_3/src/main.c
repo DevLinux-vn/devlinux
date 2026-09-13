8c263bedd411547e130150e72f0fce626d1b967d
TM-khoa <minhkhoa9a31314@gmail.com>
2026-09-13 23:56:42 +0700

    fix: remove check negative snprintf

--- embedded-linux/K26.3/truong-minh-khoa/session-02/Exercise_3/src/main.c
+++ embedded-linux/K26.3/truong-minh-khoa/session-02/Exercise_3/src/main.c
@@ -11,7 +11,7 @@ int main() {
     float a = 10.0f, b = 5.0f, c = 0.0f;
 
     ret = snprintf(msg, sizeof(msg), "calc_add: %.2f + %.2f = %.2f\n", a, b, calc_add(a, b));
-    if(ret < 0 || ret >= sizeof(msg))
+    if(ret >= sizeof(msg))
     {
         fprintf(stderr, "Buffer overflow risk with ret:%lu\n", ret);
         return 1;
@@ -20,7 +20,7 @@ int main() {
     printf("%s", msg);
 
     ret = snprintf(msg, sizeof(msg), "calc_sub: %.2f - %.2f = %.2f\n", a, b, calc_sub(a, b));
-    if(ret < 0 || ret >= sizeof(msg))
+    if(ret >= sizeof(msg))
     {
         fprintf(stderr, "Buffer overflow risk with ret:%lu\n", ret);
         return 1;
@@ -29,7 +29,7 @@ int main() {
     printf("%s", msg);
 
     ret = snprintf(msg, sizeof(msg), "calc_mul: %.2f * %.2f = %.2f\n", a, b, calc_mul(a, b));
-    if(ret < 0 || ret >= sizeof(msg))
+    if(ret >= sizeof(msg))
     {
         fprintf(stderr, "Buffer overflow risk with ret:%lu\n", ret);
         return 1;
@@ -38,7 +38,7 @@ int main() {
     printf("%s", msg);
 
     ret = snprintf(msg, sizeof(msg), "calc_div: %.2f / %.2f = %.2f\n", a, b, calc_div(a, b));
-    if(ret < 0 || ret >= sizeof(msg))
+    if(ret >= sizeof(msg))
     {
         fprintf(stderr, "Buffer overflow risk with ret:%lu\n", ret);
         return 1;
@@ -49,7 +49,7 @@ int main() {
     float div = calc_div(a, c);
     if (isnan(div)) {
         ret = snprintf(msg, sizeof(msg), "Error - Division by zero: %.2f / %.2f = %.2f\n", a, c, calc_div(a, c));
-        if(ret < 0 || ret >= sizeof(msg))
+        if(ret >= sizeof(msg))
         {
             fprintf(stderr, "Buffer overflow risk with ret:%lu\n", ret);
             return 1;
