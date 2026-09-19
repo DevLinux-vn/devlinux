6ba1cd0495dfe7d54ab18f6eb5490eef21f7b2f0
VinhNguyen-EmbeddedLinux <vinh.nguyenquang2806@gmail.com>
2026-09-19 22:23:14 +0700

    embedded-linux K26.3 session-03: hoàn thành Exercise_1, Exercise_2, Exercise_3

---
+++ embedded-linux/K26.3/nguyen-quang-vinh/session-03/Exercise_3/writer_flock.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/file.h>
+#include <time.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    char time_buf[64];
+    char log_buf[512];
+    time_t now;
+    struct tm tm_info;
+
+    if (argc != 2) {
+        printf("Usage: %s \"message text\"\n", argv[0]);
+        return 1;
+    }
+
+    /* Mở hoặc tạo file log */
+    fd = open("system.log",
+              O_WRONLY | O_APPEND | O_CREAT,
+              0644);
+
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+    /* Khóa toàn bộ file */
+    if (flock(fd, LOCK_EX) == -1) {
+        perror("flock");
+        close(fd);
+        return 1;
+    }
+
+    /* Lấy thời gian hiện tại */
+    time(&now);
+    localtime_r(&now, &tm_info);
+
+    strftime(time_buf,
+             sizeof(time_buf),
+             "%Y-%m-%d %H:%M:%S",
+             &tm_info);
+
+    /* Tạo dòng log */
+    int len = snprintf(log_buf,
+                       sizeof(log_buf),
+                       "[PID:%d] [%s] [INFO] %s\n",
+                       getpid(),
+                       time_buf,
+                       argv[1]);
+
+    /* Ghi log */
+    if (write(fd, log_buf, len) != len) {
+        perror("write");
+    }
+
+    /* Mở khóa */
+    flock(fd, LOCK_UN);
+
+    close(fd);
+
+    return 0;
+}
