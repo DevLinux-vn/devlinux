9920da13ef0eb5797ddd582106966ee4610f7912
Bui Linh <builinhhust@gmail.com>
2026-09-25 17:12:47 +0700

    C advance K26.2 Session 02: Update Exercise 1

--- c-advance/K26.2/bui-van-linh/session-02/Exercise_1/main.c
+++ c-advance/K26.2/bui-van-linh/session-02/Exercise_1/main.c
@@ -6,14 +6,16 @@ const uint32_t global_variable = 100;
 uint32_t global_data = 50;
 uint32_t global_uninit;
 
-void print_memory_map(void)
+uintptr_t addr_global_variable = (uintptr_t)&global_variable;
+
+uint8_t print_memory_map(void)
 {
     int local_init = 20;
 
     int *ptr;
     ptr = (int*)malloc(10*sizeof(int));
 
-    if(ptr = NULL) 
+    if(ptr == NULL) 
     {
         return -1;
     }
@@ -24,15 +26,10 @@ void print_memory_map(void)
     printf("HEAP address of PTR: %p\n", ptr);
     printf("STACK address of LOCAL VARIABLE: %p\n", &local_init);
 
-    uintptr_t addr_main = (uintptr_t)&print_memory_map;
-    uintptr_t addr_global_variable = (uintptr_t)&global_variable;
     uintptr_t addr_global_data = (uintptr_t)&global_data;
     uintptr_t addr_global_uninit = (uintptr_t)&global_uninit;
     uintptr_t addr_ptr = (uintptr_t)ptr;
     uintptr_t addr_local_init = (uintptr_t)&local_init;
-
-    long long delta_1 = (long long)addr_global_variable - (long long)addr_main;
-    printf("RODATA - TEXT = %lld byte\n", delta_1);
 
     long long delta_2 = (long long)addr_global_data - (long long)addr_global_variable;
     printf("DATA - TEXT = %lld byte\n", delta_2);
@@ -48,9 +45,14 @@ void print_memory_map(void)
 
 
     free(ptr);
+    return 0;
 }
 
 int main(){
+
+    uintptr_t addr_main = (uintptr_t)&main;
+    long long delta_1 = (long long)addr_global_variable - (long long)addr_main;
+    printf("RODATA - TEXT = %lld byte\n", delta_1);
 
     print_memory_map();
     return 0;
